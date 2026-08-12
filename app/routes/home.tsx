c5ac19d0dba9bd74e71d93b9ce16af874d6d4e43
Arsh <arshsingh1641@outlook.com>
2026-08-12 22:44:59 +0530

    Authentication

---
+++ app/routes/home.tsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import Navbar from "~/components/Navbar";
+import type { Route } from "./+types/home";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job" },
+  ];
+}
+
+export default function Home() {
+  const {auth} = usePuterStore();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if(!auth.isAuthenticated){
+      navigate('/auth?next=/');
+    }
+  }, [auth.isAuthenticated])
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      {/* {window.puter.ai.chat()} */}
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback.</h2>
+      </div>
+    {resumes.length> 0 && ( 
+      <div className="resumes-section">
+     {resumes.map((resume)=>(
+       <ResumeCard key={resume.id} resume={resume} />
+      ))}
+   </div>
+      )
+    }
+    </section>
+  </main>
+}
